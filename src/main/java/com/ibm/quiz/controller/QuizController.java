79215bc818f052599d44288a6efa3c9e21093a88
chinchalinchin <chinchalinchin@gmail.com>
2021-03-20 14:27:54 -0400

    add crossorigins annotation

--- src/main/java/com/ibm/quiz/controller/QuizController.java
+++ src/main/java/com/ibm/quiz/controller/QuizController.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import java.util.*;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class QuizController{
 
     @Autowired
